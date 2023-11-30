47f64448154ac1862c76a8f801c5341ab11cd164
Eugeneche <eugene.bizmail@gmail.com>
2023-11-30 20:00:04 +0100

    Set sizes changing

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,62 +1,102 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import useTranslations from "../useTranslations"
+import { LocaleContext } from "../layout"
 import * as styles from "./_Product.module.scss"
-import { GatsbyImage } from "gatsby-plugin-image"
+import "./Product.scss"
+//import { sizes } from "gatsby-plugin-sharp"
 
 const Product = () => {
 
-    const { 
-        StealthStride,
-        ArcticAura,
-        CityPulse,
-        CloudWalker
-        } = useTranslations()
-
     const data = useStaticQuery(graphql`
     query getAllProducts {
-  allStripePrice(filter: {product: {metadata: {category: {eq: "sneakers"}}}}) {
-    nodes {
-      product {
-        id
-        images
-        metadata {
-          color
-          name
-          price
+      allStripePrice(filter: {product: {metadata: {category: {eq: "sneakers"}}}}) {
+        nodes {
+          product {
+            id
+            images
+            metadata {
+              color
+              name
+              cs
+              en
+            }
+            default_price
+            name
+          }
+          unit_amount
+          nickname
         }
-        default_price
-        name
       }
-      unit_amount
-      nickname
     }
-  }
-}
     `)
 
-    const sizesRange = []
     const allProducts = data.allStripePrice.nodes
-    //const photosSlider = data.allFile.nodes
+
     const uniqueProducts = allProducts.filter((product, i, self) => {
       return i === self.findIndex((t) => {
         return t.product.id === product.product.id && t.product.id === product.product.id
       })
     })
-    //const uniqueColors = Array.from(new Set(allProducts.map(({product}) => product.metadata.color)))
-    const defaultImage = data.allStripePrice.nodes[0].product.images
-    const defaultModel = data.allStripePrice.nodes[0].product.name
 
-/*     products.forEach((prod) => {
-        console.log(prod)
+    const [ product, setProduct ] = useState(uniqueProducts[0])
+    const [ sizesRange, setSizesRange ] = useState([])
+    const [ size, setSize ] = useState(sizesRange[0])
+    const [ index, setIndex ] = useState(0)
+    //const [price, setPrice] = useState(false)
+console.log(product)
+//console.log(index)
+    const { locale } = React.useContext(LocaleContext)
+
+
+
+    const renewSizesRange = () => {
+
+      sizesRange.length = 0
+      let arr = []
+
+      allProducts.forEach(prod => {
+        if (prod.product.metadata.name === product.product.metadata.name &&
+          prod.product.metadata.color === product.product.metadata.color) {
+            arr.push(prod.nickname)
+            arr.sort((a, b) => a - b)
+          }     
+      })
+      setSizesRange(arr)
+      setSize(arr[0])
+    }
+
+    const getSize = (e, i) => {
+      e.target.getAttribute('id') === size ? 
+        e.target.setAttribute("class", "active") : 
+        e.target.setAttribute("class", "regular")
+
+      setIndex(i)
+      setSize(e.target.getAttribute('id'))
+    }
+
+    const setCurrentProduct = (e) => {
+      setProduct(uniqueProducts[e.target.id])
+    }
+
+    useEffect(() => {
+      renewSizesRange()
+    }, [product.product.metadata.color, product.product.metadata.name])
+
+/*     useEffect(() => {
+      setSize(size)
     }) */
 
-    const [ image, setImaage ] = useState(defaultImage)
-    const [ model, setModel ] = useState(defaultModel)
-    const [ currentDescription, setCurrentDescription ] = useState(ArcticAura)
+/*     const regular = {
+      border: "1px solid grey",
+      padding: "5px 10px",
+      marginRight: "5px",
+      background: "none"
+    }
 
-    //console.log(products)
-    console.log(uniqueProducts)
+ */
+    const active = {
+      background: "green"
+    }
 
     return (
         <section className={styles.section}>
@@ -64,19 +104,40 @@ const Product = () => {
                 <h2>About Product</h2>
                 <div className={styles.productInfoBlock}>
                     <div className={styles.productInfo}>
-                        <h3 className={styles.productTitle}>{defaultModel}</h3>
-                        <p>{currentDescription}</p>
+                        <h3 className={styles.productTitle}>{product.product.metadata.name}</h3>
+                        <p>{product.product.metadata[locale]}</p>
+                        <p className={styles.productPrice}>{`$${(product.unit_amount / 100).toFixed(2)}`}</p>
+                        <div className={styles.productSizes}>
+                          {sizesRange.map((size, idx) => {
+                            return (
+                              <button 
+                                id={size}
+                                onClick={(e) => getSize(e, idx)}
+                                key={size} 
+                                className={idx === index ? "active" : "regular"}
+                                /* style={} */
+                                /* className="regular" */
+                              >
+                                  {size}
+                              </button>
+                            )
+                          })}
+                        </div>
                     </div>
                     <div className={styles.productImage}>
-                        <img src={image} alt="sneakers"></img>
+                        <img src={product.product.images} alt="sneakers"></img>
                     </div>
                 </div>
-                <div className={styles.photosSlider}>
-                    {uniqueProducts.map(product => {
+                <div className={styles.photosSlider} >
+                    {uniqueProducts.map((product, i) => {
                         return (
-                            <button key={product.product.id} id={product.product.name}>
-                              <img src={product.product.images} alt="sneakers"></img>
-                            </button>
+                            <div 
+                              key={product.product.id} 
+                              
+                              onClick={(e) => setCurrentProduct(e)}
+                            >
+                              <img id={i} src={product.product.images} alt="sneakers"></img>
+                            </div>
                         )
                     })}
                 </div>
