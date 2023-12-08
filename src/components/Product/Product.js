3248383055feb8eb161e35c5c9ff56f9737a9fe1
Yevhen <53338476+Eugeneche@users.noreply.github.com>
2023-12-08 13:16:20 +0100

    Update Product.js

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -107,8 +107,8 @@ const Product = () => {
       const { error } = await stripe.redirectToCheckout({
         mode: "payment",
         lineItems: [{ price, quantity: 1 }],
-        successUrl: `${window.location.origin}/delivery`,
-        cancelUrl: `${window.location.origin}/contacts`,
+        successUrl: `/delivery`,
+        cancelUrl: `/contacts`,
       })
   
       if (error) {
